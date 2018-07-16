da50d2354b32b56a6a6230c3ed78bbb485527fb0
Francesco Gazzetta <francygazz@gmail.com>
2018-07-16 14:54:24 +0200

    Fix example package

--- pkgs/example-package/default.nix
+++ pkgs/example-package/default.nix
@@ -3,6 +3,7 @@
 stdenv.mkDerivation rec {
   name = "example-package-${version}";
   version = "1.0";
+  src = ./.;
   buildPhase = "echo echo Hello World > example";
   installPhase = "install -Dm755 example $out";
 }
