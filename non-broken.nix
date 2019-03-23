1110dde8dcdb9979bfe9481ac8722f175640e035
Francesco Gazzetta <fgaz@fgaz.me>
2019-03-23 19:03:43 +0100

    Clean up the filtering

--- non-broken.nix
+++ non-broken.nix
@@ -4,24 +4,23 @@
 # be added to the cache.
 { pkgs ? import <nixpkgs> {} }:
 
-let filterSet =
-      (f: g: s: builtins.listToAttrs
-        (map
-          (n: { name = n; value = builtins.getAttr n s; })
-          (builtins.filter
-            (n: f n && g (builtins.getAttr n s))
-            (builtins.attrNames s)
-          )
+let
+  filterSet =
+    (f: g: s: builtins.listToAttrs
+      (map
+        (n: { name = n; value = builtins.getAttr n s; })
+        (builtins.filter
+          (n: f n && g (builtins.getAttr n s))
+          (builtins.attrNames s)
         )
-      );
+      )
+    );
+  isReserved = n: builtins.elem n ["lib" "overlays" "modules"];
+  isBroken = p: ({ meta.broken = false; } // p).meta.broken;
 in filterSet
-     (n: !(n=="lib"||n=="overlays"||n=="modules")) # filter out non-packages
+     (n: !(isReserved n)) # filter out non-packages
      (p: (builtins.isAttrs p)
-       && !(
-             (builtins.hasAttr "meta" p)
-             && (builtins.hasAttr "broken" p.meta)
-             && (p.meta.broken)
-           )
+       && !(isBroken p)
      )
      (import ./default.nix { inherit pkgs; })
 
