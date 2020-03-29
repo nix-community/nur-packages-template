5da75dc99ecb9ac8b8845168a353b2dc2739b5e9
Jeremie Huchet <jeremiehuchet@users.noreply.github.com>
2020-03-29 14:07:01 +0200

    📦 add packages for gitmoji-cli and now.io

--- default.nix
+++ default.nix
@@ -14,8 +14,7 @@
   modules = import ./modules; # NixOS modules
   overlays = import ./overlays; # nixpkgs overlays
 
-  example-package = pkgs.callPackage ./pkgs/example-package { };
-  # some-qt5-package = pkgs.libsForQt5.callPackage ./pkgs/some-qt5-package { };
-  # ...
+  gitmoji-cli = (pkgs.callPackage ./pkgs/node-packages/gitmoji-cli { }).package;
+  now = (pkgs.callPackage ./pkgs/node-packages/now { }).package;
 }
 
