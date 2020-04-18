f898595e8de7d8170225b1cd3b7374105c36a954
Markus S. Wamser <github-dev@mail2013.wamser.eu>
2020-04-18 21:51:32 +0200

    add local maintainers

--- lib/default.nix
+++ lib/default.nix
@@ -1,8 +1,11 @@
 { pkgs }:
 
+let
+  upstreamMaintainers = pkgs.lib.maintainers;
+in
 with pkgs.lib; {
   # Add your library functions here
   #
   # hexint = x: hexvals.${toLower x};
+  maintainers = upstreamMaintainers // import ../maintainers.nix;
 }
-
