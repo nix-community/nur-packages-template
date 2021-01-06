4b34142d612a761e32bf6687099ec284ca1615a7
Drew Risinger <drewrisinger@users.noreply.github.com>
2021-01-06 10:11:53 -0500

    python3Packages.qdldl: init at 0.1.5

--- default.nix
+++ default.nix
@@ -59,6 +59,7 @@ rec {
     cirq = pkgs.python3.pkgs.callPackage ./pkgs/python-modules/cirq { };
     cvxpy = pkgs.python3.pkgs.callPackage ./pkgs/python-modules/cvxpy { inherit ecos osqp scs; };
     ecos = pkgs.python3.pkgs.callPackage ./pkgs/python-modules/ecos { };
+    qdldl = pkgs.python3Packages.callPackage ./pkgs/python-modules/qdldl { };
     osqp = pkgs.python3.pkgs.callPackage ./pkgs/python-modules/osqp { };
     scs = pkgs.python3.pkgs.callPackage ./pkgs/python-modules/scs { scs = lib-scs; };
 
